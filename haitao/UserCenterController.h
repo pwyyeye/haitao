4b4109349313c565673cc40c35b8519ece51a5f9
pwyyeye <pwy_ndgt@163.com>
2015-07-19 08:09:19 +0800

    usercenter

---
+++ haitao/UserCenterController.h
@@ -0,0 +1,13 @@
+//
+//  UserCenterController.h
+//  haitao
+//
+//  Created by pwy on 15/7/18.
+//  Copyright (c) 2015年 上海市配夸网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserCenterController : UITableViewController
+@property(nonatomic,strong) NSArray *tableList;
+@end
